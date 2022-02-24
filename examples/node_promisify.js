1b578b97905ad814ecbc5b0a773ca0c2276e2189
江茂琼 <jiangmaoqiong@lizhi.fm>
2022-02-24 16:30:09 +0800

    init

---
+++ examples/node_promisify.js
@@ -0,0 +1,65 @@
+// node/util/promisify.js
+// 地址：https://github1s.com/nodejs/node/blob/master/lib/internal/util.js
+
+// 以下摘取核心内容
+
+const kCustomPromisifiedSymbol = SymbolFor('nodejs.util.promisify.custom');
+const kCustomPromisifyArgsSymbol = Symbol('customPromisifyArgs');
+
+let validateFunction;
+
+function promisify(original) {
+  // Lazy-load to avoid a circular dependency.
+  // 惰性加载以避免循环依赖。
+  if (validateFunction === undefined)
+    //validateFunction作用：检查是否为function，否则抛出异常
+    ({ validateFunction } = require('internal/validators')); 
+
+  validateFunction(original, 'original');
+
+  if (original[kCustomPromisifiedSymbol]) {
+    const fn = original[kCustomPromisifiedSymbol];
+
+    validateFunction(fn, 'util.promisify.custom');
+
+    return ObjectDefineProperty(fn, kCustomPromisifiedSymbol, {
+      value: fn, enumerable: false, writable: false, configurable: true
+    });
+  }
+
+  // Names to create an object from in case the callback receives multiple arguments
+  // 在回调接收到多个时创建对象的名称
+  // e.g. ['bytesRead', 'buffer'] for fs.read.
+  const argumentNames = original[kCustomPromisifyArgsSymbol];
+
+  function fn(...args) {
+    return new Promise((resolve, reject) => {
+      ArrayPrototypePush(args, (err, ...values) => {
+        if (err) {
+          return reject(err);
+        }
+        if (argumentNames !== undefined && values.length > 1) {
+          const obj = {};
+          for (let i = 0; i < argumentNames.length; i++)
+            obj[argumentNames[i]] = values[i];
+          resolve(obj);
+        } else {
+          resolve(values[0]);
+        }
+      });
+      ReflectApply(original, this, args);
+    });
+  }
+
+  ObjectSetPrototypeOf(fn, ObjectGetPrototypeOf(original));
+
+  ObjectDefineProperty(fn, kCustomPromisifiedSymbol, {
+    value: fn, enumerable: false, writable: false, configurable: true
+  });
+  return ObjectDefineProperties(
+    fn,
+    ObjectGetOwnPropertyDescriptors(original)
+  );
+}
+
+promisify.custom = kCustomPromisifiedSymbol;
